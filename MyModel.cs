58e7ebee7fc3345b42441cb7c4b789511e3a44de
Vinay Gupta <109478270+iamrealvinnu@users.noreply.github.com>
2025-02-04 17:55:01 +0530

    Update MyModel.cs

--- MyModel.cs
+++ MyModel.cs
@@ -5,15 +5,15 @@ namespace faQClassification
     public class FAQ
     {
         [LoadColumn(0)]
-        public required string Question { get; set; }  
+        public required string Question { get; set; }
 
         [LoadColumn(1)]
-        public required string Category { get; set; }  
+        public required string Category { get; set; }
     }
 
     public class Prediction
     {
         [ColumnName("PredictedLabel")]
-        public required string PredictedCategory { get; set; }  
+        public required string PredictedCategory { get; set; }
     }
 }
